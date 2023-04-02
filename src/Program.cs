d32630ee90eb9e2d47caf3ec588efe96c9665595
Chris Meyers <thisischrismeyers@gmail.com>
2023-04-02 11:34:50 -0700

    Initial config for itemtypes

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Village
+﻿using Village.Item;
+
+namespace Village
 {
     class Program
     {
+        // Load configurations from JSON files.
+        public static void LoadConfig()
+        {
+            // Load the item types.
+            ItemType.LoadFile("config/items/itemtypes.json");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Village Entry Point");
+            //Load configs
+            LoadConfig();
+            // Print the item types.
+            foreach (ItemType itemType in ItemType.itemTypes.Values)
+            {
+                Console.WriteLine(itemType.itemType);
+            }
         }
     }
 }
